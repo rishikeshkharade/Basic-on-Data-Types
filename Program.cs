20406f2f9b9cd830e4976b279d2240077e5ebd17
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-02-04 15:13:08 +0530

    Welcome to Basic of Data Types

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Basic_on_Data_Types;
+
+class Main_Method
+{
+    static void Main()
+    {
+        Unique_Character uc = new Unique_Character();
+        uc.hasUniqueCharacters();
+            }
+}
